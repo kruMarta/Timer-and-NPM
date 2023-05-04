7b60ca708ae8cd502533316876510d7071533719
kruMarta <kruchokmarta@gmail.com>
2023-05-04 14:16:05 +0300

    -initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "airbnb",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        "linebreak-style": 0,
+        "eslint linebreak-style": [0, "error", "windows"]
+    }
+}
---
+++ script.js
@@ -0,0 +1,8 @@
+const value = 1;
+console.log(value);
+
+const hello = 'ds';
+const var2 = 2;
+if (value !== var2) {
+  console.log(hello);
+}
