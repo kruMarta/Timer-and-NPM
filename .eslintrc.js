59c28a021dab72c2213f78d6c176fb9f2f94f599
kruMarta <kruchokmarta@gmail.com>
2023-06-22 16:47:16 -0400

    done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "airbnb",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-        "linebreak-style": 0,
-        "eslint linebreak-style": [0, "error", "windows"]
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'linebreak-style': 0,
+    'eslint linebreak-style': [0, 'error', 'windows'],
+    'no-unused-vars': 'off',
+    'no-plusplus': 'off',
+    'no-undef': 'off',
+    'prefer-arrow-callback': 'off',
+    'prefer-template': 'off',
+  },
+};
--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-const value = 1;
-console.log(value);
+const time = document.getElementById('time');
 
-const hello = 'ds';
-const var2 = 2;
-if (value !== var2) {
-  console.log(hello);
+function add() {
+  time.innerHTML = parseInt(time.textContent, 10) + 1;
+}
+
+function sub() {
+  if (parseInt(time.textContent, 10) > 1) {
+    time.innerHTML = parseInt(time.textContent, 10) - 1;
+  }
+}
+
+function startCount() {
+  const toHide = document.getElementsByClassName('hide');
+  for (let i = 0; i < toHide.length; i++) {
+    const element = toHide[i];
+    element.style.opacity = '0';
+  }
+
+  const text = document.getElementById('change');
+  text.innerHTML = 'Залишилось: ';
+
+  const targetDuration = moment.duration(parseInt(time.textContent, 10), 'minutes');
+
+  const finalTime = moment().add(targetDuration);
+  const timer = setInterval(function runTimer() {
+    const currentTime = moment();
+    const remainingTime = moment.duration(finalTime.diff(currentTime));
+
+    if (remainingTime.minutes() === 0 && remainingTime.seconds() === 0) {
+      clearInterval(timer);
+    } else {
+      time.innerHTML = remainingTime.minutes() + ' : ' + remainingTime.seconds();
+    }
+  }, 990);
 }
